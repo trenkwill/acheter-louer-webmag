585c28c126a0f820a3ce6f9a26227e9e926e2a7b
William <williamhutter@me.com>
2015-01-15 20:10:41 +0100

    Built acheter-louer-3 from commit fcd6e04 on branch page-flip

--- js/main.js
+++ js/main.js
@@ -1,81 +1,6 @@
 // 'use strict';
 
-$(document).ready(function(){
 
-
-  function animateTo() {
-
-    $(function() {
-      $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top - 20
-            }, 1000);
-            return false;
-          }
-        }
-      });
-    });
-  }
-
-
-  function openAddrress() {
-    $(".open-mobile-address").click(function(){
-
-      $(".header .hide-palm").toggle();
-    });
-  }
-
-
-
-  function readmore() {
-    $(function() {
-      $('.excerpt').readmore({
-        speed: 75,
-        maxHeight: 83,
-        moreLink: '<a href="#">...</a>',
-        lessLink: '<a href="#">Fermer</a>'
-      });
-    });
-  }
-
-  function removeTargetBlankMobile() {
-    $(function(){
-      var windowWidth = $(window).width();
-      if (windowWidth < 768) {
-        $('a[target="_blank"]').removeAttr('target');
-      };
-    });
-  }
-
-
-  function fixJspContainerHeight() {
-    $(function(){
-      var windowHeight = $(window).height(),
-          jspContainer = $(".jspContainer");
-
-      jspContainer.height(windowHeight);
-      
-      $(window).resize(function(){
-          jspContainer.height(windowHeight);
-      });
-
-    }); 
-  }
-
-  
-
-  // release the kraken :
-
-  animateTo();
-  openAddrress();
-  removeTargetBlankMobile();
-  // readmore();
-
-});
 
 
 var Page = (function() {
@@ -255,5 +180,80 @@ var Page = (function() {
 
 })();
 
+$(document).ready(function(){
 
-fixJspContainerHeight();
+
+  function animateTo() {
+
+    $(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top - 20
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+  }
+
+
+  function openAddrress() {
+    $(".open-mobile-address").click(function(){
+
+      $(".header .hide-palm").toggle();
+    });
+  }
+
+
+
+  function readmore() {
+    $(function() {
+      $('.excerpt').readmore({
+        speed: 75,
+        maxHeight: 83,
+        moreLink: '<a href="#">...</a>',
+        lessLink: '<a href="#">Fermer</a>'
+      });
+    });
+  }
+
+  function removeTargetBlankMobile() {
+    $(function(){
+      var windowWidth = $(window).width();
+      if (windowWidth < 768) {
+        $('a[target="_blank"]').removeAttr('target');
+      };
+    });
+  }
+
+
+  function containerHeight() {
+    $(function(){
+      var windowHeight = $(window).height(),
+          container = $(".container");
+
+      container.height(windowHeight);
+      
+      $(window).resize(function(){
+          container.height(windowHeight);
+      });
+
+    }); 
+  }
+
+  
+
+  // release the kraken :
+
+  animateTo();
+  openAddrress();
+  removeTargetBlankMobile();
+  containerHeight();
+  // readmore();
+
+});
